cf4f5acddb17eed388436b593741b978f4294972
pragyamis <34045774+pragyamis@users.noreply.github.com>
2018-03-10 07:27:27 -0800

    Update Model.py
    Added Data Augmentation:  This trick that makes it seem like you have far more data than you actually have, resulting in even better models..

--- Model.py
+++ Model.py
@@ -20,29 +20,32 @@ my_new_model.layers[0].trainable = False
 # Which python object is being compiled? Fill in the answer so the compile command works.
 my_new_model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
-#Fit Model
+#Fit Model using data augmentation
 from tensorflow.python.keras.applications.resnet50 import preprocess_input
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 
 image_size = 224
-data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
+          
+data_generator_with_aug = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                   horizontal_flip=True,
+                                   width_shift_range = 0.2,
+                                   height_shift_range = 0.2)
 
-train_generator = data_generator.flow_from_directory(
-        directory = "../input/dogs-gone-sideways/images/train",
-        target_size=(image_size, image_size),
-        batch_size=12,
-        class_mode='categorical')
+train_generator = data_generator_with_aug.flow_from_directory(
+            '../input/dogs-gone-sideways/images/train',
+            target_size=(image_size, image_size),
+            batch_size=24,
+            class_mode='categorical')
 
-validation_generator = data_generator.flow_from_directory(
-        directory = "../input/dogs-gone-sideways/images/val",
-        target_size=(image_size, image_size),
-        batch_size = 20,
-        class_mode='categorical')
+validation_generator = data_generator_no_aug.flow_from_directory(
+            '../input/dogs-gone-sideways/images/val',
+            target_size=(image_size, image_size),
+            class_mode='categorical')
 
 my_new_model.fit_generator(
         train_generator,
-        steps_per_epoch=6,
+        steps_per_epoch=3,
+        epochs=2,
         validation_data=validation_generator,
         validation_steps=1)
-       
- 
+
